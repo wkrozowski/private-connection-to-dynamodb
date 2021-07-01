ed807419a4f69fdb57bc56d9bf4f19846fb2e7b3
wkr1u18 <wkr1u18@soton.ac.uk>
2021-07-01 12:36:26 +0200

    Add simple HTTP security group

--- main.tf
+++ main.tf
@@ -1,18 +1,63 @@
-provider "aws" {
-  region = "us-west-2"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
 }
 
-provider "random" {}
+provider "aws" {
+  region = "us-east-1"
+}
 
-resource "random_pet" "name" {}
+data "aws_availability_zones" "available" {
+  state = "available"
+}
 
-resource "aws_instance" "web" {
-  ami           = "ami-a0cfeed8"
-  instance_type = "t2.micro"
-  #user_data     = file("init-script.sh")
-  #vpc_security_group_ids = [aws_security_group.web-sg.id]
+module "vpc" {
+  name    = "my-vpc"
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.64.0"
 
-  tags = {
-    Name = random_pet.name.id
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = false
+}
+
+resource "aws_security_group" "web-sg" {
+  name = "my-web-sg"
+  vpc_id      = module.vpc.vpc_id
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+
+
+# resource "aws_instance" "data_collector_service" {
+#   ami           = "ami-830c94e3"
+#   instance_type = "t2.micro"
+
+#   tags = {
+#     Name = "DataCollectorService"
+#   }
+# }
--- terraform.tf
+++ terraform.tf
@@ -1,10 +1,1 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-    }
-    random = {
-      source = "hashicorp/random"
-    }
-  }
-}
+#
