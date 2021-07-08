575fa53efa421f51853941bb5c533cb6e829112e
wkr1u18 <wkr1u18@soton.ac.uk>
2021-07-08 17:20:51 +0200

    Blue Green deployment works

--- ecs.tf
+++ ecs.tf
@@ -54,15 +54,15 @@ resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
 }
 
 resource "aws_ecs_service" "my_first_service" {
-  name            = "my-first-service"                             # Naming our first service
-  cluster         = "${aws_ecs_cluster.data_collector_cluster.id}"             # Referencing our created Cluster
-  task_definition = "${aws_ecs_task_definition.my_first_task.arn}" # Referencing the task our service will spin up
+  name            = "my-first-service"                        # Naming our first service
+  cluster         = aws_ecs_cluster.data_collector_cluster.id # Referencing our created Cluster
+  task_definition = aws_ecs_task_definition.my_first_task.arn # Referencing the task our service will spin up
   launch_type     = "FARGATE"
   desired_count   = 3 # Setting the number of containers to 3
 
   load_balancer {
-    target_group_arn = "${aws_lb_target_group.target_group.arn}" # Referencing our target group
-    container_name   = "${aws_ecs_task_definition.my_first_task.family}"
+    target_group_arn = aws_lb_target_group.target_group.arn # Referencing our target group
+    container_name   = aws_ecs_task_definition.my_first_task.family
     container_port   = 3000 # Specifying the container port
   }
 
@@ -71,6 +71,64 @@ resource "aws_ecs_service" "my_first_service" {
     assign_public_ip = true                                                # Providing our containers with public IPs
     security_groups  = ["${aws_security_group.service_security_group.id}"] # Setting the security group
   }
+
+  deployment_controller {
+    type = "CODE_DEPLOY"
+  }
+
 }
 
+resource "aws_codedeploy_app" "data_collector" {
+  compute_platform = "ECS"
+  name             = "data_collector"
+}
 
+resource "aws_codedeploy_deployment_group" "example" {
+  app_name               = aws_codedeploy_app.data_collector.name
+  deployment_config_name = "CodeDeployDefault.ECSAllAtOnce"
+  deployment_group_name  = "example"
+  service_role_arn       = aws_iam_role.example.arn
+
+  auto_rollback_configuration {
+    enabled = true
+    events  = ["DEPLOYMENT_FAILURE"]
+  }
+
+  blue_green_deployment_config {
+    deployment_ready_option {
+      action_on_timeout = "CONTINUE_DEPLOYMENT"
+    }
+
+    terminate_blue_instances_on_deployment_success {
+      action                           = "TERMINATE"
+      termination_wait_time_in_minutes = 5
+    }
+  }
+
+  deployment_style {
+    deployment_option = "WITH_TRAFFIC_CONTROL"
+    deployment_type   = "BLUE_GREEN"
+  }
+
+  ecs_service {
+    cluster_name = resource.aws_ecs_cluster.data_collector_cluster.name
+    service_name = resource.aws_ecs_service.my_first_service.name
+  }
+
+  load_balancer_info {
+    target_group_pair_info {
+      prod_traffic_route {
+        listener_arns = [resource.aws_lb_listener.listener.arn]
+      }
+
+      target_group {
+        name = resource.aws_lb_target_group.target_group.name
+      }
+      target_group {
+        name = resource.aws_lb_target_group.blue_group.name
+      }
+
+
+    }
+  }
+}
--- lb.tf
+++ lb.tf
@@ -1,4 +1,5 @@
 resource "aws_alb" "application_load_balancer" {
+  #vpc_id = module.vpc.vpc_id
   name               = "test-lb-tf" # Naming our load balancer
   load_balancer_type = "application"
   subnets            = module.vpc.public_subnets
@@ -15,6 +16,15 @@ resource "aws_lb_target_group" "target_group" {
   vpc_id      = module.vpc.vpc_id
 }
 
+resource "aws_lb_target_group" "blue_group" {
+  name        = "blue-group"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = module.vpc.vpc_id
+}
+
+
 resource "aws_lb_listener" "listener" {
   load_balancer_arn = aws_alb.application_load_balancer.arn # Referencing our load balancer
   port              = "80"
@@ -22,5 +32,15 @@ resource "aws_lb_listener" "listener" {
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.target_group.arn # Referencing our tagrte group
+  }
+}
+
+resource "aws_lb_listener" "blue_listener" {
+  load_balancer_arn = aws_alb.application_load_balancer.arn # Referencing our load balancer
+  port              = "8080"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.blue_group.arn # Referencing our tagrte group
   }
 }
